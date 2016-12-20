b08137588246dd4e850716c8fc44ddef1fe4d77a
wei <zw454298075@qq.com>
2016-12-20 12:00:20 +0800

    经纬度7

--- AXF_zhangwei/index.php
+++ AXF_zhangwei/index.php
@@ -108,24 +108,22 @@ $signPackage = $jssdk->GetSignPackage();
      ]
   });
   wx.ready(function(){
-    var latitude, longitude;
     wx.getLocation({
       type: 'gcj02', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
       success: function (res) {
-           latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-           longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+          var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+          var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
           var speed = res.speed; // 速度，以米/每秒计
           var accuracy = res.accuracy; // 位置精度
+          wx.openLocation({
+            latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
+            longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
+            name: '', // 位置名
+            address: '', // 地址详情说明
+            scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
+            infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
+          });
       }
-    });
-    console.log(latitude + ' ' + longitude);
-    wx.openLocation({
-      latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
-      longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
-      name: '', // 位置名
-      address: '', // 地址详情说明
-      scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
-      infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
     });
     // wx.chooseWXPay({
     //   timestamp: 0, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
